b3fc53fbbecccb64e98cacdd264a918b52e548b6
Christopher J. Brody <chris.brody@gmail.com>
2018-12-12 18:24:46 -0500

    General prettier-eslint cleanup of test/xcode*.js
    (test/xcode5searchPaths.js)
    
    using npm prettier:test script

--- test/xcode5searchPaths.js
+++ test/xcode5searchPaths.js
@@ -22,33 +22,33 @@ var xcode5proj = require('./fixtures/library-search-paths'),
     proj = new pbx('.'),
     libPoop = { path: 'some/path/poop.a' };
 
-function cleanHash() {
+function cleanHash () {
     return JSON.parse(xcode5projStr);
 }
 
 exports.setUp = function (callback) {
     proj.hash = cleanHash();
     callback();
-}
+};
 
 var PRODUCT_NAME = '"$(TARGET_NAME)"';
 
 exports.addAndRemoveToFromLibrarySearchPaths = {
-    'add should add the path to each configuration section':function(test) {
+    'add should add the path to each configuration section': function (test) {
         var expected = '"\\"$(SRCROOT)/$(TARGET_NAME)/some/path\\""',
             config = proj.pbxXCBuildConfigurationSection(),
-            ref, lib, refSettings;
+            ref,
+            lib,
+            refSettings;
 
         proj.addToLibrarySearchPaths(libPoop);
 
         for (ref in config) {
-            if (ref.indexOf('_comment') > -1)
-                continue;
+            if (ref.indexOf('_comment') > -1) continue;
 
             refSettings = config[ref].buildSettings;
 
-            if (refSettings.PRODUCT_NAME != PRODUCT_NAME)
-                continue;
+            if (refSettings.PRODUCT_NAME != PRODUCT_NAME) continue;
 
             lib = refSettings.LIBRARY_SEARCH_PATHS;
             test.equal(lib[1], expected);
@@ -56,7 +56,9 @@ exports.addAndRemoveToFromLibrarySearchPaths = {
         test.done();
     },
 
-    'remove should remove from the path to each configuration section':function(test) {
+    'remove should remove from the path to each configuration section': function (
+        test
+    ) {
         var config, ref, lib;
 
         proj.addToLibrarySearchPaths(libPoop);
@@ -64,12 +66,18 @@ exports.addAndRemoveToFromLibrarySearchPaths = {
 
         config = proj.pbxXCBuildConfigurationSection();
         for (ref in config) {
-            if (ref.indexOf('_comment') > -1 || config[ref].buildSettings.PRODUCT_NAME != PRODUCT_NAME) continue;
+            if (
+                ref.indexOf('_comment') > -1 ||
+                config[ref].buildSettings.PRODUCT_NAME != PRODUCT_NAME
+            )
+                continue;
 
             lib = config[ref].buildSettings.LIBRARY_SEARCH_PATHS;
             test.ok(lib.length === 1);
-            test.ok(lib[0].indexOf('$(SRCROOT)/KitchenSinktablet/some/path') == -1);
+            test.ok(
+                lib[0].indexOf('$(SRCROOT)/KitchenSinktablet/some/path') == -1
+            );
         }
         test.done();
     }
-}
+};
